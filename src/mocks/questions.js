dd9008e1e9b60b3dfea33ef475603cd07226ac37
Rinnelis <gorgoramodus@gmail.com>
2020-06-30 11:48:40 +0300

    Добавляет аудиоплеер

--- src/mocks/questions.js
+++ src/mocks/questions.js
@@ -8,20 +8,20 @@ export default [
       src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
       genre: `rock`,
     }, {
-      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/3/32/Afineromance_by_JulesGrandgagnage.ogg`,
       genre: `blues`,
     }, {
-      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/8/83/Bodhran-improvisation_hinnerk-ruemenapf.ogg`,
       genre: `jazz`,
     }, {
-      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/a/a5/03may2020-tobefree-generated.mp3`,
       genre: `rock`,
     }],
   }, {
     type: `artist`,
     song: {
       artist: `Jim Beam`,
-      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/3/37/00_Jazz_Violin_Solo.ogg`,
     },
     answers: [{
       picture: `${AVATAR_URL}/${Math.random()}`,
